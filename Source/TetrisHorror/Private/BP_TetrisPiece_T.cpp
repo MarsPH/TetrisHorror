43f5277bd180e32e081d1b75e302abcbfad53cc1
MarsPH <m.poorhamidian@gmail.com>
2026-07-30 11:45:49 -0400

    Added: Implementation of bp tetrisboard, tetriscell and tetrispiece.

---
+++ Source/TetrisHorror/Private/BP_TetrisPiece_T.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BP_TetrisPiece_T.h"
+
---
+++ Source/TetrisHorror/Public/BP_TetrisPiece_T.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TetrisPiece.h"
+#include "BP_TetrisPiece_T.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TETRISHORROR_API ABP_TetrisPiece_T : public ATetrisPiece
+{
+	GENERATED_BODY()
+	
+};
